59313feb8bbd7b6a569177141e5ec90d01d4e9ff
RomekTuraj <roman.turaj@auroria.pl>
2021-03-30 16:11:53 +0200

    adding logging and config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,13 +4,14 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Strategy, ExtractJwt } from "passport-jwt";
 import { JwtPayload } from "./jwt-payload.interface";
 import { UserRepository } from "./user.repository";
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: "topSecret51"
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret')
     })
   }
   async validate(payload: JwtPayload) {
